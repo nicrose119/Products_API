4971fd1472cd3de553d3aff306bbb0048c67c538
Nicole Overholt <nicrose119@gmail.com>
2023-01-09 12:57:46 -0500

    Added Post and status codes to views.py plus updated settings.py, added code to change price field decimal to a string to false

--- products/views.py
+++ products/views.py
@@ -1,14 +1,18 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Products
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    products = Products.objects.all()
 
-    serializer = ProductSerializer(products, many=True)
-
-
-    return Response (serializer.data)
-    
+    if request.method == 'Get':
+        products = Products.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response (serializer.data, status = status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
